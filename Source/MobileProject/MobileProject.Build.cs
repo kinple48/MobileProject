a038c2b0ba8d5983170ca4af572b20c02062af87
kinple48 <88013706+kinple48@users.noreply.github.com>
2025-07-17 15:53:12 +0900

    lnit

---
+++ Source/MobileProject/MobileProject.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class MobileProject : ModuleRules
+{
+	public MobileProject(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule", "Niagara", "EnhancedInput" });
+    }
+}
---
+++ Source/MobileProject/MobileProject.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MobileProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MobileProject, "MobileProject" );
+
+DEFINE_LOG_CATEGORY(LogMobileProject)
+ 
